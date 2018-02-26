fa61ffda81577919f61bedd56857c810e05f2b80
chris13524 <chris@hichris.com>
2018-02-25 21:55:52 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#![no_std]
+#![feature(asm)]
+#![feature(intrinsics)]
+#![feature(lang_items)]
+#![feature(compiler_builtins_lib)]
+#![allow(unused_variables)]
+
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern fn rust_begin_panic(_msg: core::fmt::Arguments,
+                               _file: &'static str,
+                               _line: u32) -> ! {
+    loop {}
+}
